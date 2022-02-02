e96d88b163dce4d0c34b8e608dbaf05388847240
andrelouiz <andrelouiz@outlook.com>
2022-02-02 07:01:54 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Random_Number_Generators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
