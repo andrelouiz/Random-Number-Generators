c3c6b96dcaf298eb44dae2fd35f1af336e54a6ed
andrelouiz <andrelouiz@outlook.com>
2022-02-03 06:06:57 +0100

    Changes.

---
+++ ConsoleMenuElement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Random_Number_Generators
+{
+    class ConsoleMenuElement
+    {
+        public string Label { get; set; }
+        public Action ActionToRun { get; set; }
+        public ConsoleMenuElement(string label, Action actionToRun)
+        {
+            Label = label;
+            ActionToRun = actionToRun;
+        }
+        public override string ToString()
+        {
+            return Label;
+        }
+    }
+
+}
